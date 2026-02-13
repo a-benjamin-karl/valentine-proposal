07b75c6bedd7ee1e9f1868e46fa4d01f62a201c2
a-benjamin-karl <evendone771@gmail.com>
2026-02-14 00:14:28 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Your Next.js config
-};
+const nextConfig = {
+  output: 'export'
+}
 
-export default nextConfig;
+export default nextConfig
